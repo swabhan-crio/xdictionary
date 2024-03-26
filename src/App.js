0493a0c2202a5b6cbfb608ac2266aac07737ea25
B SWABHAN RAO <swabhan@Bs-MacBook-Air.local>
2024-03-26 14:16:38 +0530

    xdictionary commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [searchWord, setSearchWord] = useState("");
+  const [wordMeaning, setWordMeaning] = useState("");
+
+  const findInDictionary = () => {
+    setWordMeaning("");
+
+    const dictionary = [
+      {
+        word: "React",
+        meaning: "A JavaScript library for building user interfaces.",
+      },
+      { word: "Component", meaning: "A reusable building block in React." },
+      { word: "State", meaning: "An object that stores data for a component." },
+    ];
+
+    const lowerCaseSearchWord = searchWord.toLowerCase();
+
+    for (const entry of dictionary) {
+      const lowerCaseDictionaryWord = entry.word.toLowerCase();
+
+      if (lowerCaseSearchWord === lowerCaseDictionaryWord) {
+        setWordMeaning(entry.meaning);
+        return;
+      }
+    }
+
+    setWordMeaning((prevWordMeaning) => {
+      if (prevWordMeaning === "") {
+        return "Word not found in the dictionary.";
+      }
+      return prevWordMeaning;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dictionary App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          findInDictionary();
+        }}
+      >
+        <input
+          type="text"
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
+          placeholder="Search for a word..."
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h3 style={{ marginTop: "0px" }}>Definition:</h3>
+      <p>{wordMeaning}</p>
     </div>
   );
 }
